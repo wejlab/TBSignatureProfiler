6ce940f00b20b1013d4b6094df683139214f0ae8
Aubrey <aubreyodom55@gmail.com>
2021-03-07 12:09:32 -0500

    Comment out testing functions

--- tests/packagecoverage.R
+++ tests/packagecoverage.R
@@ -2,21 +2,21 @@
 # Run tests and generate Code Coverage Report
 
 # Look at a specific script ---------------------------------------------------
-test_for_me <- function(codefile, testfile) {
-  test_file(testfile)
-  res <- covr::file_coverage(codefile, testfile)
-  print(res)
-  covr::report(res)
-}
+#test_for_me <- function(codefile, testfile) {
+#  test_file(testfile)
+#  res <- covr::file_coverage(codefile, testfile)
+#  print(res)
+#  covr::report(res)
+#}
 
-test_for_me("R/plot.R", "tests/testthat/test-plot.R")
+#test_for_me("R/plot.R", "tests/testthat/test-plot.R")
 # Look at whole package -------------------------------------------------------
 
 # Gets percent coverage for entire package, by script
-covr::package_coverage()
-
+#covr::package_coverage()
+#
 # Report with gui (probably the best here)
-covr::report()
+#covr::report()
 
 
 
