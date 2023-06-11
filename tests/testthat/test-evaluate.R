e9b7200a7e4ace6d71545cd28cb56d4287b9cb19
Aubrey Odom <aodom@bu.edu>
2023-06-11 19:05:59 -0400

    Fix bioc warnings

--- tests/testthat/test-evaluate.R
+++ tests/testthat/test-evaluate.R
@@ -21,56 +21,6 @@ test_that("deseq2_norm_rle works", {
   )
 })
 
-test_that("LOOAUC works", {
-  expect_is(
-    LOOAUC_simple_multiple_noplot_one_df(inputTest,
-                                         targetVec = target_Vec),
-    "list"
-  )
-  expect_is(
-    Bootstrap_LOOCV_LR_AUC(inputTest,
-                           targetVec = target_Vec,
-                           nboot = 10),
-    "list"
-  )
-})
-
-test_that("signatureQuantitative works", {
-  expect_is(
-  SignatureQuantitative(inputTest,
-                        targetVec.num = target_Vec,
-                        signature.list = list(sig1 = c("gene1", "gene2", "gene3"),
-                                               sig2 = c("gene4", "gene5", "gene6")),
-                        signature.name.vec = c("sig1", "sig2"),
-                        num.boot = 3,
-                        pb.show = FALSE),
-  "list"
-  )
-  expect_error(
-    SignatureQuantitative(inputTest,
-                          targetVec.num = target_Vec,
-                          signature.list = list(sig1 = c("gene1", "gene2", "gene3"),
-                                                sig2 = c("gene4", "gene5", "gene6")),
-                          signature.name.vec = NULL,
-                          num.boot = 3,
-                          pb.show = FALSE),
-    "Please specify arguments for both signature.list and
-         signature.name.vec, or leave them both empty to use
-         TBsignatures as the list of signatures for profiling."
-  )
-  expect_error(
-    SignatureQuantitative(inputTest,
-                          targetVec.num = target_Vec,
-                          signature.list = list(sig1 = c("gene1", "gene2", "gene3"),
-                                                sig2 = c("gene4", "gene5", "gene6")),
-                          signature.name.vec = c("sig1"),
-                          num.boot = 3,
-                          pb.show = FALSE),
-    "The inputs signature.list and signature.name.vec are not the same
-         length."
-  )
-})
-
 #test_that("plotQuantitative works", {
 #  expect_is(
 #    plotQuantitative(inputTest,
